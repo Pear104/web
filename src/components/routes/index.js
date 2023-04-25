faa1dd491b3544fde92320a25a56b004f9c03681
Pear104 <schoolpear14@gmail.com>
2023-04-25 10:34:35 +0700

    hoho

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -5,7 +5,7 @@ import Playground from "../pages/Playground"
 
 export const publicRoutes = [
     { content: 'home', path: '', component: Home },
-    { content: 'about', path: process.env.PUBLIC_URL + '/about', component: About },
-    { content: 'dump', path: process.env.PUBLIC_URL + '/garbagedump', component: Garbagedump },
-    { content: 'playground', path: process.env.PUBLIC_URL + '/playground', component: Playground },
+    { content: 'about', path: '/about', component: About },
+    { content: 'dump', path: '/garbagedump', component: Garbagedump },
+    { content: 'playground', path: '/playground', component: Playground },
 ]
